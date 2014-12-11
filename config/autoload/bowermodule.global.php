84f2be27e760ebae05d9bc0e85c53d43dbd2dca4
Aleksandr <schurix@gmx.de>
2014-12-11 13:36:50 +0100

    Initial Commit with bower, composer and less config

---
+++ config/autoload/bowermodule.global.php
@@ -0,0 +1,37 @@
+<?php
+return array(
+    'bower' => array(
+        'bower_folder' => array(
+            'os' => 'bower_components',
+        ),
+        'pack_folder' => array(
+            'os' => 'public/js',
+            'web' => '/js',
+        ),
+        'debug_folder' => array(
+            'os' => 'public/js/dev',
+            'web' => '/js/dev',
+        ),
+        'debug_mode' => true,
+		'packs' => array(
+			'main' => array(
+				'token' => 'main',
+				'modules' => array(
+					'jquery',
+					'bootstrap',
+					"fontawesome",
+				)
+			),
+			'ieLT9' => array(
+				'token' => 'ieLT9',
+				'modules' => array(
+					'html5shiv',
+					'respond',
+				),
+				'attributes' =>  array(
+					'conditional' => 'lt IE 9',
+				),
+			)
+		)
+    ),
+);
