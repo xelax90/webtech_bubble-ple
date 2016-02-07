5eb5e1a92f8f9c3f81521c13b67b8b247ff708a7
Aleksandr <schurix@gmx.de>
2016-02-07 19:34:02 +0100

    Added posX, posY and priority to bubbles. New route updatePositions

--- module/BubblePle/src/BubblePle/Form/BubbleFieldset.php
+++ module/BubblePle/src/BubblePle/Form/BubbleFieldset.php
@@ -43,6 +43,52 @@ class BubbleFieldset extends Fieldset implements InputFilterProviderInterface, O
 			)
 		));
 
+		$this->add(array(
+			'name' => 'posX',
+			'type' => 'Number',
+			'options' => array(
+				'label' => gettext_noop('Pos X'),
+				'column-size' => 'sm-10',
+				'label_attributes' => array(
+					'class' => 'col-sm-2',
+				),
+			),
+			'attributes' => array(
+				'id' => "",
+				'step' => '1',
+			)
+		));
+		
+		$this->add(array(
+			'name' => 'posY',
+			'type' => 'Number',
+			'options' => array(
+				'label' => gettext_noop('Pos Y'),
+				'column-size' => 'sm-10',
+				'label_attributes' => array(
+					'class' => 'col-sm-2',
+				),
+			),
+			'attributes' => array(
+				'id' => "",
+				'step' => '1',
+			)
+		));
+		
+		$this->add(array(
+			'name' => 'priority',
+			'type' => 'Text',
+			'options' => array(
+				'label' => gettext_noop('Priority'),
+				'column-size' => 'sm-10',
+				'label_attributes' => array(
+					'class' => 'col-sm-2',
+				),
+			),
+			'attributes' => array(
+				'id' => "",
+			)
+		));
 	}
 
 	public function getInputFilterSpecification() {
@@ -56,7 +102,29 @@ class BubbleFieldset extends Fieldset implements InputFilterProviderInterface, O
 					array('name' => 'XelaxHTMLPurifier\Filter\HTMLPurifier'),
 				),
 			),
-
+			
+			'posX' => array(
+				'required' => false,
+				'filters' => array(
+					array('name' => 'Int'),
+				),
+			),
+			
+			'posY' => array(
+				'required' => false,
+				'filters' => array(
+					array('name' => 'Int'),
+				),
+			),
+			
+			'priority' => array(
+				'required' => false,
+				'filters' => array(
+					array('name' => 'StringTrim'),
+					array('name' => 'StripTags'),
+					array('name' => 'XelaxHTMLPurifier\Filter\HTMLPurifier'),
+				),
+			),
 		);
 		return $filters;
 	}
