2f88377320bd460d5f58d3dd6dde138cbdfbd67a
touhid1259 <touvan1259@gmail.com>
2016-02-08 20:22:10 +0100

    adding skip button

--- public/js/angular/scripts/controller/loginCtrl.js
+++ public/js/angular/scripts/controller/loginCtrl.js
@@ -12,11 +12,13 @@ app.controller('loginCtrl', ['$location', '$scope', '$http', '$mdToast', functio
     $scope.noticeMessageShow = false;
     $scope.noticeMessage = 'Please authorize the app from L2P';
     $scope.sync = false;
+    $scope.skipL2pSync = false;
 
 
     $http.get('l2p/authenticate').then(function (response) {
         if (response.data['success'] === true) {
             $scope.sync = true;
+            $scope.skipL2pSync = true;
             $scope.authenticate = false;
             $scope.noticeMessageShow = true;
             $scope.noticeMessage = 'You are already authenticated. Sync your L2P.';
@@ -42,6 +44,7 @@ app.controller('loginCtrl', ['$location', '$scope', '$http', '$mdToast', functio
                     $http.get('l2p/authenticate').then(function (next_response) {
                         if (next_response.data['success'] === true) {
                             $scope.sync = true;
+                            $scope.skipL2pSync = true;
                             $scope.noticeMessage = 'You are authenticated. Sync your L2P.';
                             $scope.loading = false;
                             clearTimeout(refreshPage);
@@ -63,6 +66,7 @@ app.controller('loginCtrl', ['$location', '$scope', '$http', '$mdToast', functio
     $scope.l2pSync = function () {
         console.log('syncing');
         $scope.sync = false;
+        $scope.skipL2pSync = false;
         $scope.loading = true;
         $scope.loadingMessage = 'Syncing...';
         $scope.noticeMessage = 'L2P is syncing...it may take 1 to 2 minutes';
@@ -79,14 +83,20 @@ app.controller('loginCtrl', ['$location', '$scope', '$http', '$mdToast', functio
                 }, 1500);
             } else if (response.data['success'] === false) {
                 $scope.sync = true;
+                $scope.skipL2pSync = false;
                 $scope.loading = false;
                 $scope.noticeMessage = 'Error Syncing...Please try again';
 
             }
         }, function (errResponse) {
             $scope.sync = true;
+            $scope.skipL2pSync = true;
             $scope.loading = false;
             $scope.noticeMessage = 'Error Syncing...Please try again';
         });
     };
+    
+    $scope.skipToHomepage = function(){
+        $location.path('/bubbles');
+    }
 }]);
